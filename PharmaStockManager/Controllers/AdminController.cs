1ae0a6d896ed820d4db9317deefd5cccac19767e
sonergunes741 <sonergunes741@gmail.com>
2024-10-09 00:05:41 +0300

    Add AdminController for user management and Users view

---
+++ PharmaStockManager/Controllers/AdminController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading.Tasks;
+
+[Authorize(Roles = "Admin")] // Bu controller'a yalnızca Admin rolündeki kullanıcılar erişebilir.
+public class AdminController : Controller
+{
+    private readonly UserManager<IdentityUser> _userManager;
+
+    public AdminController(UserManager<IdentityUser> userManager)
+    {
+        _userManager = userManager;
+    }
+
+    // GET: Admin/Users
+    public IActionResult Users()
+    {
+        var users = _userManager.Users.ToList(); // Mevcut tüm kullanıcıları listele
+        return View(users);
+    }
+}
