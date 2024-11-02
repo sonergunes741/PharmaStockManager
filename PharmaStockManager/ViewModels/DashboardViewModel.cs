4802e0a54392ff75aa96df5965751a69bd6cca83
Hüseyin Çetin <112018788+hcetin2021@users.noreply.github.com>
2024-11-02 16:18:45 +0300

    DashboardViewModels.cs added

---
+++ PharmaStockManager/ViewModels/DashboardViewModel.cs
@@ -0,0 +1,10 @@
+// ViewModels/DashboardViewModel.cs
+namespace PharmaStockManager.ViewModels
+{
+    public class DashboardViewModel
+    {
+        public int TotalCategories { get; set; }
+        public int TotalProducts { get; set; }
+        public int LowStockProducts { get; set; }
+    }
+}
