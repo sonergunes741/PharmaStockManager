4640a15b434296e6c3d099c77ff377776a6c8d3a
AhmetSadriG <ahmetsadriguler@hotmail.com>
2024-12-07 19:04:46 +0300

    index sayfası değişikliği

--- PharmaStockManager/Controllers/AccountController.cs
+++ PharmaStockManager/Controllers/AccountController.cs
@@ -331,7 +331,7 @@ namespace StockManager.Controllers
         public async Task<IActionResult> Logout()
         {
             await _signInManager.SignOutAsync();
-            return RedirectToAction("Login", "Account");
+            return RedirectToAction("Index", "Home");
         }
 
         [HttpGet]
