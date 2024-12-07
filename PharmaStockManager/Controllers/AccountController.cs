01538b67fc206d8a40255d5b6cc888c691dc4200
AhmetSadriG <ahmetsadriguler@hotmail.com>
2024-12-07 18:57:40 +0300

    Revert "index sayfası"
    This reverts commit 2b51bba1480dcc313aaf7e8a2598cc4cc060299d.

--- PharmaStockManager/Controllers/AccountController.cs
+++ PharmaStockManager/Controllers/AccountController.cs
@@ -331,7 +331,7 @@ namespace StockManager.Controllers
         public async Task<IActionResult> Logout()
         {
             await _signInManager.SignOutAsync();
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("Login", "Account");
         }
 
         [HttpGet]
