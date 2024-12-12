ab2d18417cdaedb7e6e93daff2f5ac052d08ec0c
Failk <kaestr15@gmail.com>
2024-12-12 17:51:26 +0300

    warehouse

---
+++ PharmaStockManager/Models/ManageUsersViewModel.cs
@@ -0,0 +1,16 @@
+﻿using PharmaStockManager.Models.Identity;
+namespace PharmaStockManager.Models
+{
+    public class ManageUsersViewModel
+    {
+        public IList<AppUser_Warehouse> AdminUsers { get; set; }
+        public IList<AppUser_Warehouse> EmployeeUsers { get; set; }
+        public IList<AppUser_Warehouse> CustomerUsers { get; set; }
+    }
+
+    public class AppUser_Warehouse : AppUser
+    {
+        public string WarehouseName { get; set; }
+    }
+
+}
