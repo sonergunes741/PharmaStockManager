db1ce9f50b6dac3ef7f566ac6367945469777164
Failk <kaestr15@gmail.com>
2024-12-12 17:44:34 +0300

    Update Program.cs

--- PharmaStockManager/Program.cs
+++ PharmaStockManager/Program.cs
@@ -56,19 +56,6 @@ using (var scope = app.Services.CreateScope())
         await roleManager.CreateAsync(new AppRole(customerRole));
     }
 
-    // Create User role if it doesn't exist
-    var employeeRole = "Employee";
-    if (!await roleManager.RoleExistsAsync(employeeRole))
-    {
-        await roleManager.CreateAsync(new AppRole(employeeRole));
-    }
-
-    var customerRole = "Customer";
-    if (!await roleManager.RoleExistsAsync(customerRole))
-    {
-        await roleManager.CreateAsync(new AppRole(customerRole));
-    }
-
     // Create a default admin user if it doesn't exist
     var superAdminEmail = "webwizardssol@example.com";
     var superAdminUser = await userManager.FindByEmailAsync(superAdminEmail);
@@ -129,36 +116,6 @@ using (var scope = app.Services.CreateScope())
         };
         await userManager.CreateAsync(customerUser, "Customer123!"); // Default password
         await userManager.AddToRoleAsync(customerUser, customerRole);
-    }
-
-
-    // Create a default employee if it doesn't exist
-    var employeeEmail = "employee@example.com";
-    var defaultEmployee = await userManager.FindByEmailAsync(employeeEmail);
-    if (defaultEmployee == null)
-    {
-        defaultEmployee = new AppUser
-        {
-            ActiveUser = true,
-            UserName = employeeEmail,
-            Email = employeeEmail
-        };
-        await userManager.CreateAsync(defaultEmployee, "Employee123!"); // Default password
-        await userManager.AddToRoleAsync(defaultEmployee, employeeRole);
-    }
-
-    var customerEmail = "customer@example.com";
-    var defaultCustomer = await userManager.FindByEmailAsync(customerEmail);
-    if (defaultCustomer == null)
-    {
-        defaultCustomer = new AppUser
-        {
-            ActiveUser = true,
-            UserName = customerEmail,
-            Email = customerEmail
-        };
-        await userManager.CreateAsync(defaultCustomer, "Customer123!"); // Default password
-        await userManager.AddToRoleAsync(defaultCustomer, customerRole);
     }
 }
 
